86c29af3304ccd4bfc57fd8de2de952658ed09de
diahpuspita <diah.puspitadc111@gmail.com>
2022-11-21 13:10:09 +0700

    yy

--- resources/views/landingpage/blog.blade.php
+++ resources/views/landingpage/blog.blade.php
@@ -100,7 +100,7 @@
         </div>
     </section>
 </body>
-@include('landingpage.layout.footerhal')
+@include('landingpage.layout.footerr')
 <script src="{{ asset('1/yeye/html/acadu/demo/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('1/yeye/html/acadu/demo/assets/js/app.min.js') }}"></script>
 <script src="{{ asset('1/yeye/html/acadu/demo/assets/js/main.js') }}"></script>
